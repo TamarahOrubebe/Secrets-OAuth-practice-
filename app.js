91cb69d9b308e413b165728db4f18a254107f90a
Tamarah <paulorubebe@yahoo.com>
2020-09-07 18:36:48 +0100

    Update Google ID and Secret

--- app.js
+++ app.js
@@ -63,19 +63,22 @@ passport.deserializeUser(function (id, done) {
 	});
 });
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-},
-    function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-));
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:3000/auth/google/secrets",
+			// userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+		},
+		function (accessToken, refreshToken, profile, cb) {
+			console.log(profile);
+			User.findOrCreate({ googleId: profile.id }, function (err, user) {
+				return cb(err, user);
+			});
+		}
+	)
+);
 
 
 
